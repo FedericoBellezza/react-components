bb57f4e7729b0a23f55d0611a2901f7ff31abe50
FedericoBellezza <federico.bellezza2001@gmail.com>
2024-11-28 15:56:16 +0100

    add main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 // import { useState } from "react";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import Main from "./components/Main/Main";
 
 function App() {
   return (
     <>
-      <Header></Header>
-      <Main></Main>
+      <div className="page-container d-flex flex-column justify-content-between">
+        <Header></Header>
+
+        <Main></Main>
+        <Footer></Footer>
+      </div>
     </>
   );
 }
